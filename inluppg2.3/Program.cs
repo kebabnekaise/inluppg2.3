7023447e9646e25385044520cb872f2c1c08cf6e
emsi06001 <emsi06001@E-PW0234N3>
2023-09-13 13:15:14 +0200

    Add project files.

---
+++ inluppg2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace inluppg2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startavgift = 300;
+            int krpkm = 1;
+            int prisPerDag = 500;
+
+            Console.WriteLine("Hur många dagar tänker du hyra?");
+            int dagar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur lång tänker du köra?");
+            int km = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Det kommer att kosta dig {startavgift + (dagar-1)*prisPerDag + km*krpkm}:-");
+        }
+    }
+}
